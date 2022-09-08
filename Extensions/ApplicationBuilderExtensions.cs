1064c5e1ba6f3bcdb4a0da19176fa2119981dc7a
Aren Chen <gucci313031@gmail.com>
2022-09-08 15:16:47 +0800

    register endpoint

--- Extensions/ApplicationBuilderExtensions.cs
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -1,18 +1,32 @@
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 namespace Netcorext.Extensions.Swagger.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
-    public static IApplicationBuilder UseSwagger(this IApplicationBuilder builder, string name, string documentUrl)
+    public static IApplicationBuilder UseSwagger(this IApplicationBuilder builder, string name, string documentRoutePrefix = "/docs", string documentRoutePattern = "/docs/{*remainder}", string swaggerJsonRoutePattern = "/docs/{documentName}/swagger.{json|yaml}")
     {
         var app = (WebApplication)builder;
-        
-        app.MapSwagger(documentUrl + "/{documentName}/swagger.json");
 
-        app.UseSwaggerUI(options =>
-                         {
-                             options.SwaggerEndpoint("v1/swagger.json", name);
-                             options.RoutePrefix = documentUrl;
-                         });
+        app.MapSwagger(swaggerJsonRoutePattern);
+
+        using var scope = app.Services.CreateScope();
+
+        var options = scope.ServiceProvider.GetRequiredService<IOptionsSnapshot<SwaggerUIOptions>>().Value;
+
+        options.SwaggerEndpoint("v1/swagger.json", name);
+        options.RoutePrefix = documentRoutePrefix.TrimStart('/');
+
+        options.ConfigObject.Urls ??= new[] { new UrlDescriptor { Name = "V1 Docs", Url = "v1/swagger.json" } };
+
+        var endpoints = (IEndpointRouteBuilder)app;
+
+        var pipeline = endpoints.CreateApplicationBuilder()
+                                .UseMiddleware<SwaggerUIMiddleware>(options)
+                                .Build();
+
+        endpoints.MapGet(documentRoutePattern, pipeline);
 
         return app;
     }
